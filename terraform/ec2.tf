fef642124c3a002e9a84dd1b7324028496e76345
PoojanShah <takkeen685@gmail.com>
2025-11-11 19:23:29 -0500

    changed the jenkins playbook to accomodate github ssh key feature and rerun bug

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "example_server" {
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   root_block_device {
-    volume_size           = 20    # 20 GB root volume
+    volume_size           = 30
     volume_type           = "gp3" # General purpose SSD
     delete_on_termination = true
 
